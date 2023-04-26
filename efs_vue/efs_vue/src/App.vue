8e9684b41942512321d8e9d8dcba9d737467de73
Cameron Croghan <cameron.croghan@gmail.com>
2023-04-25 22:19:07 -0500

    Home, Register, customers, and investments are go. Working on stocks.

--- efs_vue/efs_vue/src/App.vue
+++ efs_vue/efs_vue/src/App.vue
@@ -5,8 +5,14 @@
             <router-link to="/">Home</router-link> |
         </li>
         <li class="nav-item">
-            <router-link :to="{name: 'CustomerList'}">Customers</router-link>
+            <router-link :to="{name: 'CustomerList'}">Customers</router-link>|
         </li>
+        <li class="nav-item">
+            <router-link :to="{name: 'InvestmentList'}">Investments</router-link>
+        </li>|
+        <li class="nav-item">
+            <router-link :to="{name: 'StockList'}">Stocks</router-link>
+        </li>|
         <li class="nav-item" v-if="!authenticated" @click="login" >
             | <router-link :to="{name: 'Auth'}">Log in</router-link>
         </li>
@@ -33,6 +39,8 @@
             menu: [
                 { title: 'Home', url:"/"},
                 { title: 'Customers', url:"/customer-list" },
+                { title: 'Investments', url:"/investment-list" },
+                { title: 'Stocks', url:"/stock-list" },
             ]
         }),
         mounted() {
--- efs_vue/efs_vue/src/router/index.js
+++ efs_vue/efs_vue/src/router/index.js
@@ -1,9 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+  import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/components/Home.vue'
 import Auth from '@/components/Auth.vue'
 import Register from '@/components/Register'
 import CustomerList from '@/components/CustomerList'
 import CustomerCreate from '@/components/CustomerCreate'
+import InvestmentList from '@/components/InvestmentList'
+import InvestmentCreate from '@/components/InvestmentCreate'
+import StockList from '@/components/StockList.vue'
+import StockCreate from '@/components/StockCreate.vue';
 
 const routes = [
   {
@@ -40,6 +44,46 @@ const routes = [
     component: CustomerCreate
   },
   {
+    path: '/investment-list',
+    name: 'InvestmentList',
+    component: InvestmentList
+  },
+  {
+    path: '/investment-list/:msg',
+    name: 'InvestmentUpdatedList',
+    component: InvestmentList
+  },
+  {
+    path: '/investment-create',
+    name: 'InvestmentCreate',
+    component: InvestmentCreate
+  },
+  {
+    path: '/investment-create/:pk',
+    name: 'InvestmentUpdate',
+    component: InvestmentCreate
+  },
+  {
+    path: '/stock-list',
+    name: 'StockList',
+    component: StockList
+  },
+  {
+    path: '/stock-list/:msg',
+    name: 'StockUpdatedList',
+    component: StockList
+  },
+  {
+    path: '/stock-create',
+    name: 'StockCreate',
+    component: StockCreate
+  },
+  {
+    path: '/stock-create/:pk',
+    name: 'StockUpdate',
+    component: StockCreate
+  },
+  {
     path: '/auth',
     name: 'Auth',
     component: Auth
@@ -48,7 +92,7 @@ const routes = [
     path:'/register',
     name: 'Register',
     component: Register
-  }
+  } 
 ]
 
 
